4ccc1268b730f1eea36a249f172e6edd6a5dc6ba
Cristian Ramirez <cristiano.rosetti@gmail.com>
2023-01-10 12:15:17 -0600

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -8,75 +8,75 @@ provider "google" {
   zone    = var.zone
 }
 
-#resource "google_compute_network" "hashistack" {
-#  name = "hashistack-${var.name}"
-#}
+resource "google_compute_network" "hashistack" {
+ name = "hashistack-${var.name}"
+}
 
-# resource "google_compute_firewall" "consul_nomad_ui_ingress" {
-#   name          = "${var.name}-ui-ingress"
-#   network       = "harness-se-latam" #google_compute_network.hashistack.name
-#   source_ranges = [var.allowlist_ip]
+resource "google_compute_firewall" "consul_nomad_ui_ingress" {
+  name          = "${var.name}-ui-ingress"
+  network       = google_compute_network.hashistack.name #"harness-se-latam" #google_compute_network.hashistack.name
+  source_ranges = [var.allowlist_ip]
 
-#   # Nomad
-#   allow {
-#     protocol = "tcp"
-#     ports    = [4646]
-#   }
+  # Nomad
+  allow {
+    protocol = "tcp"
+    ports    = [4646]
+  }
 
-#   # Consul
-#   allow {
-#     protocol = "tcp"
-#     ports    = [8500]
-#   }
-# }
+  # Consul
+  allow {
+    protocol = "tcp"
+    ports    = [8500]
+  }
+}
 
-# resource "google_compute_firewall" "ssh_ingress" {
-#   name          = "${var.name}-ssh-ingress"
-#   network       = "harness-se-latam" #google_compute_network.hashistack.name
-#   source_ranges = [var.allowlist_ip]
+resource "google_compute_firewall" "ssh_ingress" {
+  name          = "${var.name}-ssh-ingress"
+  network       = google_compute_network.hashistack.name #"harness-se-latam" #google_compute_network.hashistack.name
+  source_ranges = [var.allowlist_ip]
 
-#   # SSH
-#   allow {
-#     protocol = "tcp"
-#     ports    = [22]
-#   }
-# }
+  # SSH
+  allow {
+    protocol = "tcp"
+    ports    = [22]
+  }
+}
 
-# resource "google_compute_firewall" "allow_all_internal" {
-#   name        = "${var.name}-allow-all-internal"
-#   network     = "harness-se-latam" #google_compute_network.hashistack.name
-#   source_tags = ["auto-join"]
+resource "google_compute_firewall" "allow_all_internal" {
+  name        = "${var.name}-allow-all-internal"
+  network     = google_compute_network.hashistack.name #"harness-se-latam" #google_compute_network.hashistack.name
+  source_tags = ["auto-join"]
 
-#   allow {
-#     protocol = "icmp"
-#   }
+  allow {
+    protocol = "icmp"
+  }
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["0-65535"]
-#   }
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
 
-#   allow {
-#     protocol = "udp"
-#     ports    = ["0-65535"]
-#   }
-# }
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+}
 
-# resource "google_compute_firewall" "clients_ingress" {
-#   name          = "${var.name}-clients-ingress"
-#   network       = "harness-se-latam" #google_compute_network.hashistack.name
-#   source_ranges = [var.allowlist_ip]
-#   target_tags   = ["nomad-clients"]
+resource "google_compute_firewall" "clients_ingress" {
+  name          = "${var.name}-clients-ingress"
+  network       = google_compute_network.hashistack.name #"harness-se-latam" #google_compute_network.hashistack.name
+  source_ranges = [var.allowlist_ip]
+  target_tags   = ["nomad-clients"]
 
-#   # Add application ingress rules here
-#   # These rules are applied only to the client nodes
+  # Add application ingress rules here
+  # These rules are applied only to the client nodes
 
-#   # nginx example; replace with your application port
-#   allow {
-#     protocol = "tcp"
-#     ports    = [80]
-#   }
-# }
+  # nginx example; replace with your application port
+  allow {
+    protocol = "tcp"
+    ports    = [80]
+  }
+}
 
 resource "google_compute_instance" "server" {
   count        = var.server_count
@@ -95,7 +95,7 @@ resource "google_compute_instance" "server" {
   }
 
   network_interface {
-    network = "harness-se-latam" #google_compute_network.hashistack.name
+    network = google_compute_network.hashistack.name #"harness-se-latam" #google_compute_network.hashistack.name
     access_config {
       // Leave empty to get an ephemeral public IP
     }
@@ -137,7 +137,7 @@ resource "google_compute_instance" "client" {
   }
 
   network_interface {
-    network = "harness-se-latam" #google_compute_network.hashistack.name
+    network = google_compute_network.hashistack.name #"harness-se-latam" #google_compute_network.hashistack.name
     access_config {
       // Leave empty to get an ephemeral public IP
     }
